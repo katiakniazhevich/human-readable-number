07cd46879f4a1b69338f7cadeba8e50de5bd42d1
katiakniazhevich <55781001+katiakniazhevich@users.noreply.github.com>
2021-03-15 22:30:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    if (number == 0) {
+        return 'zero';
+    }
+    let ten = parseInt(number % 100 / 10);
+    const hundred = parseInt(number / 100),
+        one = parseInt(number % 10),
+        oneName = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
+        tenName = ['', '', 'twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety '],
+        hundredName = ['', 'one hundred ', 'two hundred ', 'three hundred ', 'four hundred ', 'five hundred ', 'six hundred ', 'seven hundred ', 'eight hundred ', 'nine hundred '];
+
+    if (ten == 1) {
+        ten = number % 100;
+        arrNumber = hundredName[hundred] + oneName[ten];
+    } else {
+        arrNumber = hundredName[hundred] + tenName[ten] + oneName[one];
+    }
+
+    return arrNumber.trim();
+};
